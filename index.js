b3fc5c6961fe9dbff1a4b71942ed39d095f1002c
CamShaft <bytheway.cameron@gmail.com>
2014-01-14 23:06:24 -0700

    update ng-hyper

--- index.js
+++ index.js
@@ -1,118 +1,161 @@
 /**
- * Initialize the environment
- */
-
-var envs = require('envs');
-if (window.env) envs.set(window.env);
-
-/**
  * Module dependencies
  */
 
-var angular = require('angular');
+var angular = window.angular || require('/CamShaft-component-angular');
 var hyper = require('ng-hyper');
 var feature = require('ng-feature');
+var translate = require('ng-hyper-translate');
 var logger = require('./lib/logger');
-var hyperagent = require('hyperagent');
 var token = require('access-token');
-var each = require('each');
+var envs = require('envs');
 
 /**
- * Expose creating an app
+ * Create a simple-ui app
+ *
+ * @param {String} mod
+ * @param {Array} deps
+ * @param {Function} require
+ * @return Module
  */
 
-exports = module.exports = function(mod, deps) {
+exports = module.exports = function(mod, deps, $require) {
   if (!deps) return angular.module(mod);
+  if (!$require) throw new Error('simple-ui requires passing the require function on creation');
+
   deps.push(hyper.name);
+  deps.push(translate.name);
   deps.push(feature.name);
+
   var app = angular.module(mod, deps);
   app.name = mod;
+
+  app.initPartial = initRequire('../partials/', $require);
+  initRoutes(app, $require);
+
+  app.initDirective = initRequire('./directives/', $require, app);
+  app.initController = initRequire('./controllers/', $require, app);
+  app.initFilter = initRequire('./filters/', $require, app);
+  app.initService = initRequire('./services/', $require, app);
+
+  app.metric = initMetric(app);
+  // TODO init analytics
+  initHttp(app);
+
+  app.start = initStart(app, $require);
+
   return app;
 };
 
 /**
- * Start an app with options
+ * Initialize routes
  */
 
-exports.run = function(app, options, loadPartial) {
+function initRoutes(app, $require) {
+  var routes = $require('./routes');
 
-  // initialize the logger
-  var log = window.metric = logger(app.name, {
-    collector: options.collector || envs('SYSLOG_COLLECTOR'),
-    context: options.context || {}
-  });
-
-  // initialize the hyperagent client
-  hyperagent.set(token.auth());
-  hyperagent.profile = log.profile.bind(log);
-
-  // TODO initialize in-progress
-  // TODO initialize subscriptions
-
-  // set the default routes
-  var routes = options.routes || {'/': 'index'};
-
-  // initialize the routes
-  loadPartial = options.loader || loadPartial;
-  var routeConfig = {};
-  each(routes, function(path, opts) {
-    if (typeof opts === 'string') opts = {templateUrl: opts, _route: opts};
-    // load the template
-    if (loadPartial && opts.templateUrl) opts.templateUrl = loadPartial(opts.templateUrl);
-
-    routeConfig[path] = opts;
+  // we have to require the partials before the config runs
+  var config = {};
+  angular.forEach(routes, function(opts, path) {
+    if (angular.isString(opts)) opts = {templateUrl: opts, _route: opts};
+    if (opts.templateUrl) opts.templateUrl = app.initPartial(opts.templateUrl);
+    config[path] = opts;
   });
 
   app.config([
     '$routeProvider',
     '$locationProvider',
-
-    function($routeProvider, $locationProvider) {
-      each(routeConfig, function(path, opts) {
-        // Handle a catch all here
-        if (path === '_') return $routeProvider.otherwise(opts);
-
-        $routeProvider.when(path, opts);
+    function($route, $location) {
+      angular.forEach(config, function(opts, path) {
+        if (path === '_') return $route.otherwise(opts);
+        $route.when(path, opts);
       });
-      $locationProvider.html5Mode(true).hashPrefix('!');
+      $location.html5Mode(true).hashPrefix('!');
     }
   ]);
+  return routes;
+}
 
-  app.run([
-    '$rootScope',
-    '$location',
+/**
+ * Helper function for creating initializers
+ */
 
-    function($rootScope, $location) {
-      var done;
+function initRequire(prefix, $require, app) {
+  return function(path) {
+    var mod = $require(prefix + path);
+    if (app) return mod(app);
+    return mod;
+  };
+}
 
-      $rootScope.$on('$routeChangeStart', function() {
-        done = log.profile('route_time');
-      });
+/**
+ * Initialize performance metrics
+ *
+ * @todo
+ */
 
-      $rootScope.$on('$routeChangeSuccess', function(currentRoute, conf) {
-        if (options.analytics) analytics.pageview();
-        if (currentRoute.title) $rootScope.title = currentRoute.title;
-        if (conf.$$route._route) $rootScope._route = conf.$$route._route;
-        done({path: $location.path()});
+function initMetric(app) {
+  var log = logger(app.name, {
+    collector: envs('SYSLOG_COLLECTOR')
+  });
+
+  return log;
+}
+
+/**
+ * Initialize business metrics
+ *
+ * @todo
+ */
+
+function initAnalytics() {
+  return {
+    pageview: function() {}
+  };
+}
+
+/**
+ * Initialize the $http module
+ */
+
+function initHttp(app) {
+  app.config([
+    '$provide',
+    '$httpProvider',
+    function ($provide, $http) {
+      var api = envs('API_URL');
+      if (api) $provide.value('hyperHttpRoot', api);
+
+      // TODO redirect to login if the token is no longer valid
+      $http.defaults.transformRequest.push(function(data, get) {
+        var headers = get();
+        headers['Authorization'] = token.bearer();
+        return data;
       });
     }
   ]);
+}
 
-  /**
-   * Show the feature UI
-   */
+/**
+ * Initialize start function
+ */
 
-  setTimeout(function() {
-    require('feature-ui')(options.feature);
+function initStart(app) {
+  return function start() {
+    app.run([
+      '$rootScope',
+      '$location',
+      function($rootScope, $location) {
+        $rootScope.$on('$routeChangeSuccess', function(currentRoute, conf) {
+          if (currentRoute.title) $rootScope.title = currentRoute.title;
+          if (conf && conf.$$route && conf.$$route._route) $rootScope._route = conf.$$route._route;
+        });
+      }
+    ]);
 
-    if (!window.performance || !window.performance.timing) return;
-    var t = performance.timing;
-    var timing = {
-      'measure#performance.connect': t.connectEnd - t.connectStart + 'ms',
-      'measure#performance.domain': t.domainLookupEnd - t.domainLookupStart + 'ms',
-      'measure#performance.response': t.responseEnd - t.requestStart + 'ms',
-      'measure#performance.dom': t.domComplete - t.domLoading + 'ms'
-    };
-    log(timing);
-  }, 0);
+    // show the feature UI
+    setTimeout(function() {
+      require('feature-ui')();
+    }, 10);
+  };
 }
