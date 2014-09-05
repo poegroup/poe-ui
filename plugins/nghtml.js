fb06d7394c2222902b7d5bc4dfae25f8f7abc80f
camshaft <bytheway.cameron@gmail.com>
2014-09-05 15:52:07 -0600

    add support for plain html partials

--- plugins/nghtml.js
+++ plugins/nghtml.js
@@ -4,15 +4,17 @@
 
 var nghtml = require('nghtml');
 var jade = require('jade');
+var extname = require('path').extname;
 
 module.exports = function(builder) {
   var features = [];
 
   nghtml({
     webroot: 'public',
-    extension: '.jade',
+    extension: ['.jade', '.html'],
     confProp: 'angular-templates',
     hook: function (content, filename) {
+      if (extname(filename) !== '.jade') return content;
       var opts = {
         filename: filename
       };
