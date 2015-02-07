f7fee49c5b61d715f7385299556af495d3f15bf7
camshaft <bytheway.cameron@gmail.com>
2015-02-07 14:59:11 -0700

    use ess instead of rework

--- server.js
+++ server.js
@@ -47,8 +47,8 @@ exports = module.exports = function(routesPath, opts) {
   app.builder.resolve.extensions.push('.jade');
   app.builder.addLoader('jade', (process.env.NODE_ENV === 'development' ? 'react-component-loader!' : '') + 'esnext-loader!onus-loader!ast2template-loader!jade2ast-loader');
 
-  app.builder.addLoader(/\.(ess\?(dynamic|raw))$/, 'ess-loader!esnext-loader!ast2template?pass-through=1!rework2ast-loader');
-  app.builder.addStyle(/\.(ess)$/, 'css-loader!ess-loader!esnext-loader!ast2template?pass-through=1!rework2ast-loader');
+  app.builder.addLoader(/\.(ess\?(dynamic|raw))$/, 'ess-loader!esnext-loader!ast2template?pass-through=1!ess2ast-loader');
+  app.builder.addStyle(/\.(ess)$/, 'css-loader!ess-loader!esnext-loader!ast2template?pass-through=1!ess2ast-loader');
 
   app.builder.addLoader('yml', 'json-loader!yaml-loader');
 
