e9b3d35f0bb10d7790cf8e2504f62bb79c773a9a
mndvns <mail@mndvns.com>
2015-02-25 07:58:36 -0700

    add autoprefixer-loader

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ exports = module.exports = function(routesPath, opts) {
   app.builder.addLoader('jade', (process.env.NODE_ENV === 'development' ? 'react-component-loader!' : '') + 'esnext-loader!onus-loader!ast2template-loader!jade2ast-loader');
 
   app.builder.addLoader(/\.(ess\?(dynamic|raw))$/, 'ess-loader!esnext-loader!ast2template?pass-through=1!ess2ast-loader');
-  app.builder.addStyle(/\.(ess)$/, 'css-loader!ess-loader!esnext-loader!ast2template?pass-through=1!ess2ast-loader');
+  app.builder.addStyle(/\.(ess)$/, 'css-loader!autoprefixer-loader!ess-loader!esnext-loader!ast2template?pass-through=1!ess2ast-loader');
 
   app.builder.addLoader('yml', 'json-loader!yaml-loader');
 
